3d8e024e44484ac6fa3efb0864d7f034208fee52
usmanhameed2017 <usmanhameed1790+backup@gmail.com>
2025-12-25 14:11:26 +0500

    Reuse db connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,28 +3,16 @@ import mongoose from "mongoose";
 // Mongo url
 const MONGO_URL = `${process.env.MONGO_URL}/${process.env.DB_NAME}`;
 
-if(!global._mongoose) global._mongoose = { conn: null, promise: null };
-
 // Connect
 export const connectDB = async () => {
-    if(global._mongoose.conn) return global._mongoose.conn;
-
-    if(!global._mongoose.promise) 
-    {
-        global._mongoose.promise = mongoose.connect(MONGO_URL, {
-            bufferCommands: false,
-        });
-    }
-
+    if(mongoose.connection.readyState === 1) return;
     try 
     {
-        global._mongoose.conn = await global._mongoose.promise;
-        console.log("MongoDB connected");
-        return global._mongoose.conn;
+        const response = await mongoose.connect(MONGO_URL);
+        console.log(`Database connected to ${response.connection.host}`);
     } 
     catch(error) 
     {
-        global._mongoose.promise = null;
         console.error("MongoDB connection failed:", error.message);
         throw error;
     }
