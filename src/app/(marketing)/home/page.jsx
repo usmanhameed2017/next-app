cb8b30f08c9cf240db37638ac833d7ebae1fffe1
usmanhameed2017 <usmanhameed1790+backup@gmail.com>
2025-12-26 18:42:21 +0500

    server only code

--- src/app/(marketing)/home/page.jsx
+++ src/app/(marketing)/home/page.jsx
@@ -1,18 +1,16 @@
-import api from '@/service/axios';
 import React from 'react';
 
 export const revalidate = 30;
 
 async function Home() 
 {
-    const response = await api.get({ url:"/products?page=1&limit=100" });
-    const products = response.data.docs;
+    const { data } = await (await fetch(`http://localhost:8000/api/v1/products?page=1&limit=100`)).json();
+    const products = data.docs;
     return (
         <>
             <h2 className='bg-dark text-white text-center py-5 fw-bold'> HOME PAGE </h2>
             <div className="container mt-5">
                 <div className="row">
-                    
                     {products.map(product => (
                         <div className="col-md-4" key={product._id}>
                             <div className="card shadow m-1">
